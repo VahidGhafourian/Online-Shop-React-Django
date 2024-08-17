7ed1f8a623fee8318e399a9b6002ece6403cb001
vahid1378518 <vahid1378518@gmail.com>
2024-08-17 22:42:19 +0330

    add create super user command to account. edit dockerfile

--- Dockerfile
+++ Dockerfile
@@ -12,14 +12,7 @@ COPY ./drf /app
 
 RUN python manage.py collectstatic --noinput
 
-# Set environment variables for Django superuser creation
-ENV DJANGO_SUPERUSER_PHONENUMBER=vahid
-ENV DJANGO_SUPERUSER_EMAIL=vahid@example.com
-ENV DJANGO_SUPERUSER_PASSWORD=vahid
-
-# Script to create a superuser if it doesn't already exist
-RUN echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SUPERUSER_PHONENUMBER', '$DJANGO_SUPERUSER_PASSWORD', '$DJANGO_SUPERUSER_EMAIL',) if not User.objects.filter(phone_number='$DJANGO_SUPERUSER_PHONENUMBER').exists() else print('Superuser already exists.')" | python manage.py shell
-
+RUN python manage.py createsu
 
 RUN cd /app
 CMD ["gunicorn", "--bind", "0.0.0.0:8000", "OnlineShop.wsgi:application"]
---
+++ drf/account/managemet/commands/createsu.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+class Command(BaseCommand):
+    help = 'Create a superuser with environment variables'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        if not User.objects.filter(phone_number=os.getenv('DJANGO_SUPERUSER_PHONENUMBER')).exists():
+            User.objects.create_superuser(
+                phone_number=os.getenv('DJANGO_SUPERUSER_PHONENUMBER'),
+                email=os.getenv('DJANGO_SUPERUSER_EMAIL'),
+                password=os.getenv('DJANGO_SUPERUSER_PASSWORD'),
+            )
+            self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists'))
