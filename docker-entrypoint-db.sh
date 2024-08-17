6b0fadb145a4ebbfa974745eb7f2ba83374c22c7
vahid1378518 <vahid1378518@gmail.com>
2024-08-17 19:24:15 +0330

    update docker-compose.yml

--- docker-entrypoint-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Replace environment variables in SQL template
-envsubst < /docker-entrypoint-initdb.d/init_db_template.sql > /docker-entrypoint-initdb.d/init_db.sql
-
-# Execute the original entrypoint script of PostgreSQL
-exec docker-entrypoint.sh postgres
