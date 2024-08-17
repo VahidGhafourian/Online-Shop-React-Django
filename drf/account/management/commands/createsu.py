2b8d496e64111846e7bdf2c38b124aa0e3b53094
vahid1378518 <vahid1378518@gmail.com>
2024-08-17 23:39:05 +0330

    edit createsu. copy env to app in docker file

--- drf/account/management/commands/createsu.py
+++ drf/account/management/commands/createsu.py
@@ -1,18 +1,22 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 import os
+from dotenv import load_dotenv
+
 
 class Command(BaseCommand):
     help = 'Create a superuser with environment variables'
 
     def handle(self, *args, **options):
+        env_path = './.env'
+        load_dotenv(dotenv_path=env_path)
         User = get_user_model()
-        if not User.objects.filter(phone_number=os.environ.get('DJANGO_SUPERUSER_PHONENUMBER')).exists():
-            print(os.environ.get('DJANGO_SUPERUSER_PHONENUMBER'))
+        if not User.objects.filter(phone_number=os.getenv('DJANGO_SUPERUSER_PHONENUMBER')).exists():
+            print(os.getenv('DJANGO_SUPERUSER_PHONENUMBER'))
             User.objects.create_superuser(
-                phone_number=os.environ.get('DJANGO_SUPERUSER_PHONENUMBER'),
-                email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
-                password=os.environ.get('DJANGO_SUPERUSER_PASSWORD'),
+                phone_number=os.getenv('DJANGO_SUPERUSER_PHONENUMBER'),
+                email=os.getenv('DJANGO_SUPERUSER_EMAIL'),
+                password=os.getenv('DJANGO_SUPERUSER_PASSWORD'),
             )
             self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
         else:
