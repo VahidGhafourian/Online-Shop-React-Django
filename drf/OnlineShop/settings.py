fba4e4fd2552f0a057c3e339b4fc0b8a1cc8b037
vahid1378518 <vahid1378518@gmail.com>
2024-08-18 16:30:58 +0330

    update settings

--- drf/OnlineShop/settings.py
+++ drf/OnlineShop/settings.py
@@ -101,8 +101,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'online_shop'),
-        'USER': os.getenv('DB_USER', 'django'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'admin'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
